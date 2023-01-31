fd96c44d5d341318df7ca6651312d5ed691dbe2a
Alessandra Murat <alessandra.murat@outlook.com>
2023-01-31 08:17:58 -0300

    Utilizando class com stateful

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import {Component} from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-  render () {
-    return(
-      <h1>Hello World!</h1>
-    )
+  // constructor(props) {
+  //   super(props);
+  //   // this.handlePClick = this.handlePClick.bind(this); para substituir essa linha basta passar uma arrow function para o método
+  //   this.state = {
+  //     name: "Alessandra Murat",
+  //     counter: 0,
+  //   };
+  // }
+  //para eliminar o constructor podemos utilizar o class field que permite criar atributos na classe sem depender do constructor
+
+  state = {
+    name: "Alessandra Murat",
+    counter: 0,
+  };
+
+  handlePClick = () => {
+    this.setState({ name: "Alessandra Gabriele" });
+  };
+
+  handleLinkClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    // const name = this.state.name;
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleLinkClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o Link
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
